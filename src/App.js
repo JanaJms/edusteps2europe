a93437cdfabc2fa8b93fc970338963f28d7d033a
JanaJms <janajamousdev@gmail.com>
2025-11-23 11:23:50 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ComingSoon from './Views/ComingSoon';
+
+function App() {
+  return (
+    <div className="App w-full min-h-screen">
+      <ComingSoon/>
+    </div>
+  );
+}
+
+export default App;
