18d8486ee5958d92276e695030ea5027372da90c
JanaJms <janajamousdev@gmail.com>
2025-11-30 17:34:28 +0200

    add component of hero section

--- src/components/views/home/HeroSection.js
+++ src/components/views/home/HeroSection.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import bgHeroImage from "../../../assets/images/hero.jpg"
+import Button from '../../buttons/Button';
 
 export default function HeroSection() {
 
@@ -9,7 +10,7 @@ export default function HeroSection() {
         CONTAINER: " w-[90%] sm:w-full bottom-[10px] left-6 md:left-8 md:bottom-[40px] lg:bottom-[35px] lg:left-[55px] 2xl:bottom-4 2xl:left-14 z-[3] absolute items-start flex flex-col gap-y-3.5",
         TEXT_COMP: "max-w-[238px] sm:max-w-[400px] md:max-w-[43%]",
         TEXT: "text-[24px] font-bold md:text-[32px] lg:text-[48px] text-light-gray font-playfair ",
-        TEXT_2: "text-[22px] sm:text-[32px] md:text-[48px] lg:text-[56px] lg:leading-[64px] text-wrap text-light-gray font-medium",
+        BTNS: " flex gap-x-5 font-poppins font-medium",
     }
 
     return (
@@ -28,13 +29,23 @@ export default function HeroSection() {
 
             <div className={classStyles.CONTAINER}>
                 <div className={classStyles.TEXT_COMP}>
-                    <div data-aos="fade-right">
+                    <div data-aos="fade-right" className='flex flex-col gap-y-5'>
                         <h1 className={classStyles.TEXT}>
                             Bring Your Child Closer to a Brighter Future in France
                         </h1>
-                        <h2 className={classStyles.TEXT_2}>
-                            hi
+                        <h2 className={classStyles.BTNS}>
+                            <Button
+                                customizeStyle={`bg-light-orange text-white-smoke`}
+                                buttonText={`Book Free Consultation`} />
+
+                            <Button
+                                customizeStyle={`text-blue bg-grey-goose`}
+                                buttonText={`See How It Works`} />
                         </h2>
+                        <Button
+                            textSize={`14px`}
+                            buttonText={`The First Step Starts Here – Download Your Free Guide to Studying in France`}
+                            customizeStyle='text-start text-grey-goose underline font-bold cursor-pointer !px-0' />
                     </div>
                 </div>
             </div>
