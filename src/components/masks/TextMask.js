e5fa279a00db649ff04519862e5ff533f6f8af0e
JanaJms <janajamousdev@gmail.com>
2025-12-16 20:09:59 +0200

    update text mask component

--- src/components/masks/TextMask.js
+++ src/components/masks/TextMask.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
-export default function TextMask({ text, image }) {
+export default function TextMask({ text, image, classStyle }) {
     return (
         <div
             className={`
-                text-[300px] font-extrabold font-poppins
-                bg-center bg-contain text-transparent bg-clip-text
+                text-[120px] md:text-[160px] lg:text-[200px] font-extrabold font-poppins w-fit h-fit
+                bg-center bg-contain text-transparent bg-clip-text leading-none
+                ${classStyle}
             `}
             style={{
                 backgroundImage: `url(${image})`
