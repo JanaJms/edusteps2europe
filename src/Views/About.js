0e633f8816faa8d149814d09153bf96593b0b30d
JanaJms <janajamousdev@gmail.com>
2025-12-25 13:46:01 +0200

    vision

--- src/Views/About.js
+++ src/Views/About.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../components/views/about/Hero'
+import Vision from '../components/views/about/Vision'
 
 export default function About() {
   return (
     <div className='flex flex-col w-full gap-y-8 md:gap-y-20 pb-12 md:pb-28'>
         <Hero/>
+        <Vision/>
     </div>
   )
 }
---
+++ src/components/views/about/Vision.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Vision() {
+    return (
+        <div className=' flex flex-col px-4 md:px-10 lg:px-20 gap-6 lg:gap-10'>
+            <div className='font-playfair font-semibold text-[22px] md:text-[30px] lg:text-[44px] max-w-[50vw]'>
+                Our vision is to transform
+                your study abroad experience
+            </div>
+            <div className='text-[12px] md:text-[16px]'>
+                and become the most trusted, student-focused support platform for international students in France. We aim to make studying abroad a well-guided, stress-free, and rewarding journey. By offering personalized resources, expert guidance, and a supportive community, we empower students to thrive academically and personally in their new environment.
+            </div>
+        </div>
+    )
+}
