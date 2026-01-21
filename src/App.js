61ac76106a5dc9e8fec30ed5db26698b7f0075fa
JanaJms <janajamousdev@gmail.com>
2026-01-21 20:57:43 +0200

    implement routing

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Outlet, Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Views/About';
 import ContactUs from './Views/ContactUs';
@@ -5,11 +6,22 @@ import FAQ from './Views/FAQ';
 import Home from './Views/Home';
 import Packages from './Views/Packages';
 import Services from './Views/Services';
+import Appartments from './Views/Appartments';
 
 function App() {
   return (
     <div className="App w-full min-h-screen">
-      <ContactUs />
+      <Routes>
+        <Route path="/" element={<Outlet />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="services" element={<Services />} />
+          <Route path="packages" element={<Packages />} />
+          <Route path="faq" element={<FAQ />} />
+          <Route path="contact" element={<ContactUs />} />
+          <Route path="appartments" element={<Appartments />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Views/Appartments.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Appartments() {
+  return (
+    <div>Appartments</div>
+  )
+}
