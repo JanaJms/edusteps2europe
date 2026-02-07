c84a04c8f092a37a9ae10b89a9ced0af6b9fdddd
JanaJms <janajamousdev@gmail.com>
2026-02-07 14:19:21 +0200

    fixes

--- src/components/views/services/ServicesSection.js
+++ src/components/views/services/ServicesSection.js
@@ -1,47 +1,63 @@
-import React from 'react'
+import essentials from "../../../assets/images/05a6d621-8957-4329-a843-b516a5a915d0.jpeg"
+import translations from "../../../assets/images/74872337-52c4-48e4-86df-641f65f3b8fe.jpeg"
+import storage from "../../../assets/images/ac51ae2f-feab-421f-bf1f-7183e833e01d.jpeg"
+import comparision from "../../../assets/images/bfb4d4c9-3cda-4562-9e20-68bb4bebe808.jpeg"
+import integration from "../../../assets/images/f2d6d9df-d0d7-4038-85dc-be8f0a33e04b.jpeg"
+import local from "../../../assets/images/Gare du Nord, Paris Railway Station… (1).jpeg"
+import bank from "../../../assets/images/download (1).jpeg"
+import costs from "../../../assets/images/fd0ce4fa-8176-40bb-93c0-4a73f8f31797.jpeg"
+import job from "../../../assets/images/Foto jovem gerente feminina feliz segura….jpeg"
+import passport from "../../../assets/images/Obtenez la nationalité française en 5 étapes.jpeg"
+import crous from "../../../assets/images/Screenshot 2025-06-22 181751.png"
+import assurance from "../../../assets/images/Screenshot 2025-06-22 181819.png"
+import caf from "../../../assets/images/Screenshot 2025-06-22 181901.png"
+import texting from "../../../assets/images/SEND TEXTS_ Message your friend as often as….jpeg"
+import vls from "../../../assets/images/THE IMPORTANCE OF ACCURACY IN LEGAL TRANSLATION.jpeg"
+import phone from "../../../assets/images/This Phone Hack Can Save You Hundreds on Your Next….jpeg"
+import housing from "../../../assets/images/When renting out Student Apartments in Mobile, we….jpeg"
 
 export default function ServicesSection() {
     const data = [
         {
             title: "University & Academics",
             list: [
-                { title: "University comparison", description: "Rankings, career prospects, locations", imgSrc: "" },
-                { title: "University life integration", description: "Student associations, library registration, social events", imgSrc: "" },
-                { title: "Student jobs", description: "20h/week rules, CV help", imgSrc: "" },
+                { title: "University comparison", description: "Rankings, career prospects, locations", imgSrc: comparision },
+                { title: "University life integration", description: "Student associations, library registration, social events", imgSrc: integration },
+                { title: "Student jobs", description: "20h/week rules, CV help", imgSrc: job },
             ],
         },
         {
             title: "Housing & Essentials",
             list: [
-                { title: "Housing", description: "Help contacting/choosing residences", imgSrc: "" },
-                { title: "Essential purchases", description: "Kettle, cutlery, duvet, pillows, sheets, hygiene products", imgSrc: "" },
-                { title: "Storage support", description: "Luggage saving during vacation or moving", imgSrc: "" },
+                { title: "Housing", description: "Help contacting/choosing residences", imgSrc: housing },
+                { title: "Essential purchases", description: "Kettle, cutlery, duvet, pillows, sheets, hygiene products", imgSrc: essentials },
+                { title: "Storage support", description: "Luggage saving during vacation or moving", imgSrc: storage },
             ],
         },
         {
             title: "Legal & Administrative",
             list: [
-                { title: "VLS validation", description: "Help contacting/choosing residences", imgSrc: "" },
-                { title: "Residence permit", description: "Guidance for validation and administrative steps", imgSrc: "" },
-                { title: "French translations", description: "Document translation support", imgSrc: "" },
-                { title: "CROUS", description: "Account setup and submission", imgSrc: "" },
-                { title: "AMELI", description: "Social security enrollment", imgSrc: "" },
-                { title: "CAF", description: "Account creation for housing aid", imgSrc: "" },
-                { title: "Bank account", description: "Support with opening a French bank account", imgSrc: "" },
-                { title: "Phone subscriptions", description: "Guidance for mobile plans", imgSrc: "" },
+                { title: "VLS validation", description: "Help contacting/choosing residences", imgSrc: vls },
+                { title: "Residence permit", description: "Guidance for validation and administrative steps", imgSrc:passport },
+                { title: "French translations", description: "Document translation support", imgSrc: translations },
+                { title: "CROUS", description: "Account setup and submission", imgSrc: crous },
+                { title: "AMELI", description: "Social security enrollment", imgSrc: assurance },
+                { title: "CAF", description: "Account creation for housing aid", imgSrc: caf },
+                { title: "Bank account", description: "Support with opening a French bank account", imgSrc: bank },
+                { title: "Phone subscriptions", description: "Guidance for mobile plans", imgSrc: phone },
             ],
         },
         {
             title: "Local Life Setup",
             list: [
-                { title: "Local transport", description: "Navigo pass, public transport guidance", imgSrc: "" },
-                { title: "Living costs", description: "Estimated monthly expenses by city", imgSrc: "" },
+                { title: "Local transport", description: "Navigo pass, public transport guidance", imgSrc: local },
+                { title: "Living costs", description: "Estimated monthly expenses by city", imgSrc: costs },
             ],
         },
         {
             title: "Ongoing Support",
             list: [
-                { title: "WhatsApp assistance", description: "Real-time help anytime", imgSrc: "" },
+                { title: "WhatsApp assistance", description: "Real-time help anytime", imgSrc: texting },
             ],
         },
     ];
