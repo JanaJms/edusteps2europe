8a92b90154726006c26375396c5155ee0cf2194a
JanaJms <janajamousdev@gmail.com>
2026-02-24 21:20:59 +0200

    fix i18n config

--- src/i18n.js
+++ src/i18n.js
@@ -1,29 +1,26 @@
-// src/i18n.js
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
+import enTranslations from './locals/en.json';
+import frTranslations from './locals/fr.json';
+
 const resources = {
     en: {
-        translation: {
-            "welcome": "Welcome to EduSteps2Europe",
-            // add your English strings here
-        }
+        translation: enTranslations
     },
     fr: {
-        translation: {
-            "welcome": "Bienvenue chez EduSteps2Europe",
-            // add your French strings here
-        }
+        translation: frTranslations
     }
 };
 
 i18n
-    .use(initReactI18next)
+    .use(initReactI18next) // passes i18n down to react-i18next
     .init({
         resources,
-        lng: "en", // default language
+        lng: "en", // default language to read
+        fallbackLng: "en", // if a key is missing in FR, use the EN version
         interpolation: {
-            escapeValue: false
+            escapeValue: false // react already safes from xss
         }
     });
 
